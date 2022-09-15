b3e83c5de8df2db7e4d5f7b4373bc50f5ddab873
BattaMahi <maheshbatta14@gmail.com>
2022-09-15 18:14:38 +0530

    added source

---
+++ src/main/java/com/mahesh/mahesh.java
@@ -0,0 +1,19 @@
+package com.mahesh;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class mahesh {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("maheshbatta14@gmail.com");
+		
+	}
+
+}
